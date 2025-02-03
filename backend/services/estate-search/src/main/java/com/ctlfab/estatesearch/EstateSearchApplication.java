5774803aea2d1de2644c1d84384ecbbb6a20c04a
Fabrizio Ciotola <fabrizio.ciot@gmail.com>
2025-02-03 23:42:27 +0100

    Aggiunta struttura cartelle

---
+++ backend/services/estate-search/src/main/java/com/ctlfab/estatesearch/EstateSearchApplication.java
@@ -0,0 +1,13 @@
+package com.ctlfab.estatesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateSearchApplication.class, args);
+	}
+
+}
